4abb0338667294a2f03efcd710b12283c092fe5b
Kemal bayram <kemalbayramag@gmail.com>
2023-06-21 17:00:17 +0300

    json olarak gönderilen bodyleri algılaması için @RequestBody annotationu kullanıldı

--- src/main/java/com/example/product_app/controller/ProductController.java
+++ src/main/java/com/example/product_app/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.example.product_app.model.service.AbstractProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -20,7 +17,7 @@ public class ProductController
     @Autowired
     private AbstractProductService productService;
     @PostMapping
-    public ResponseEntity<Product> save(Product product){
+    public ResponseEntity<Product> save(@RequestBody Product product){
         product.setCreatedDate(new Date());
         Product savedProduct = productService.save(product);
         if (savedProduct != null) return ResponseEntity.ok(savedProduct);
