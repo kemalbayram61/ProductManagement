7d369324b12b83fde5918fcfa69bfbfaa28044dc
Kemal bayram <kemalbayramag@gmail.com>
2023-06-21 16:33:43 +0300

    product controller içerisine liste metodu eklendi

--- src/main/java/com/example/product_app/controller/ProductController.java
+++ src/main/java/com/example/product_app/controller/ProductController.java
@@ -5,23 +5,31 @@ import com.example.product_app.model.service.AbstractProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("api/product")
 public class ProductController
 {
     @Autowired
     private AbstractProductService productService;
-    @PostMapping(path = "/save")
+    @PostMapping
     public ResponseEntity<Product> save(Product product){
         product.setCreatedDate(new Date());
         Product savedProduct = productService.save(product);
         if (savedProduct != null) return ResponseEntity.ok(savedProduct);
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll(){
+        List<Product> productList = productService.getAll();
+        return ResponseEntity.ok(productList);
     }
 }
