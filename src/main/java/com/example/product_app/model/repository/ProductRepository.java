222a679c86fa5fdcb3ff163a1417bc05db0c9226
Kemal bayram <kemalbayramag@gmail.com>
2023-06-21 14:40:24 +0300

    repository JpaRepositoriden kalıtım aldı ve özel metotlar eklendi

--- src/main/java/com/example/product_app/model/repository/ProductRepository.java
+++ src/main/java/com/example/product_app/model/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package com.example.product_app.model.repository;
 
-public class ProductRepository {
+import com.example.product_app.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    // SELECT * FROM PRODUCTS WHERE PRICE >= ?
+    List<Product> findByPriceGreaterThanEqual(double salary);
+
+    // SELECT * FROM PRODUCTS WHERE PRICE > ?
+    List<Product> findByPriceGreaterThan(double salary);
 }
